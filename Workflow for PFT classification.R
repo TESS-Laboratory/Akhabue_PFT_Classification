dc97cdcbee094e4c0cfa59f89cb513035fe6d1e9
Enimhien <akhasfaith@gmail.com>
2025-05-28 18:17:12 +0100

    update

--- Workflow for PFT classification.R
+++ Workflow for PFT classification.R
@@ -1,37 +1,4 @@
-# Pipeline----
-#  ✓ read in lookup data for categorical traits
-#  ✓ clean the data by removing the columns you don't need.
-#  ✓ read in trait data
-# sort by species, to determine the species you have and how many species your observation covers
-
-
-{ # list out the different PFTs in JULES
-# BET-Tr (tropical broadleaf evergreen trees), 
-# BET-Te (temperate broadleaf evergreen trees), 
-# BDT (broadleaf deciduous trees), 
-# NET (needle-leaf evergreen trees), 
-# NDT (needle-leaf deciduous trees), 
-# ESh (evergreen shrubs), 
-# DSh (deciduous shrubs),
-# C3 grasses, 
-# C4 grasses, 
-}
-
-# ✓ Assign each specie a PFT class based on the information from the lookup table
-# sort the assigned species according to the species from your work data - as these are the species you will be working with
-# merge the new df - the pft classification data - to match with your working data 
-
-
-
-
-
-
-
-
-
-# Code--------------------------------------------
-
-# Load packages ----
+# Load packages and library----
 library(tidyverse)
 library(viridis)  # For color scales
 library(sf)
